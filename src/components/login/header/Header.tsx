d3872b20ca53e4d754d7f2a487e94df19e9e1d61
Yulchikss <yuliapelia19@gmail.com>
2023-12-22 15:58:51 +0200

    feat: settings project

---
+++ src/components/login/header/Header.tsx
@@ -0,0 +1,17 @@
+// src/components/MyComponent.tsx
+
+import React from 'react';
+
+// interface MyComponentProps {
+//   name: string;
+// }
+
+const Header = () => {
+  return <header className='header' >
+    <div className="container">
+        <a href="/" className='logo'>BR</a>
+    </div>
+  </header>;
+};
+
+export default Header;
